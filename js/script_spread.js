95806f4656c2439c43b0286fd78b92b2c4e02333
Alex <example@gmail.com>
2022-07-11 15:17:15 +0500

    new commit 2/36

--- js/script_spread.js
+++ js/script_spread.js
@@ -217,22 +217,34 @@ const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
 function availableCurr(arr, missingCurr) {
     let str = '';
     arr.length === 0 ? str = 'Нет доступных валют' : str = 'Доступные валюты:\n';
-
-    arr.forEach(function(curr, i) {
-        if (curr !== missingCurr) {
-            str += `${curr}\n`;
-        }
-    });
-
-    // Или
-    // for (let i = 0; i < arr.length; i++) {
-    //     if (arr[i] === missingCurr) {
-    //         continue;
+	
+	// if (arr.length === 0) {
+	// 	str = 'Нет доступных валют';
+	// } else {
+	// 	str = 'Доступные валюты:\n';
+	// }		
+    // arr.forEach(function(curr, i) {
+    //     if (curr !== missingCurr) {
+    //         str += `${i} ${curr} \n`;
     //     }
-    //     str += `${arr[i]}\n`;
-    // }
+    // });
+	
+    // Или
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === missingCurr) {
+            continue;
+        }
+        str += `${i+1} ${arr[i]}\n`;
+    }
 
     return str;
 }
 console.log(availableCurr([...baseCurrencies, ...additionalCurrencies]));
+
 ///////////////////////////////////////////////////////////////////////
+
+
+let stir = 'aaa ddd ffff';
+console.log(stir);
+let stir2 = stir.split('').reverse().join('');
+console.log(stir2);
