45014e014c31f586e3832f4f3608905e9e22d1c3
Alex <example@gmail.com>
2022-07-11 05:18:19 +0500

    new commit 2/36

--- js/script_spread.js
+++ js/script_spread.js
@@ -198,3 +198,41 @@ console.log(showProgrammingLangs(personalPlanPeter));
 
 ////////////////////////////////////////////////////////
 
+//// part 2
+
+let someString = 'This is some strange string';
+function reverse(str) {
+	if (typeof(str) !== 'string') {
+		return 'Ошибка!';
+	}
+	return str.split('').reverse().join('');
+}
+console.log(reverse(someString));
+
+///////////////////////////////////////////////////////
+
+const baseCurrencies = ['USD', 'EUR'];
+const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
+
+function availableCurr(arr, missingCurr) {
+    let str = '';
+    arr.length === 0 ? str = 'Нет доступных валют' : str = 'Доступные валюты:\n';
+
+    arr.forEach(function(curr, i) {
+        if (curr !== missingCurr) {
+            str += `${curr}\n`;
+        }
+    });
+
+    // Или
+    // for (let i = 0; i < arr.length; i++) {
+    //     if (arr[i] === missingCurr) {
+    //         continue;
+    //     }
+    //     str += `${arr[i]}\n`;
+    // }
+
+    return str;
+}
+console.log(availableCurr([...baseCurrencies, ...additionalCurrencies]));
+///////////////////////////////////////////////////////////////////////
