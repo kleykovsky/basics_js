67da5120b407dd338050e215b25048a71195570a
Alex <kleykovsky@gmail.com>
2022-08-11 09:32:55 +0500

    add_new_file

---
+++ js/rest.js
@@ -0,0 +1,10 @@
+const log = function(a, b, ...rest) {
+    console.log(a, b, rest);
+};
+
+log(1, 2, '3');
+
+function calcOrDouble(number, basis = 2) {
+    console.log(number * basis);
+}
+calcOrDouble(5, 3);
