1c98dea1c04a392a8ba8644aa9d44b81e69b2d85
Alex <kleykovsky@gmail.com>
2022-08-13 12:28:02 +0500

    rev: 2.0

--- js/AJAX.js
+++ js/AJAX.js
@@ -9,9 +9,14 @@ inputSom.addEventListener('input', () => {
     request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
     request.send();
 
-    request.addEventListener('readystatechange', () => {
-        if (request.readyState === 4 && request.status === 200) {
-            console.log(request.response);
+    request.addEventListener('load', () => {
+        if (request.status === 200) {
+            // console.log(request.response);
+            const data = JSON.parse(request.response);
+            inputUsd.value = (+inputSom.value / data.current.usd).toFixed(2);
+        } else {
+            inputUsd.value = '***Error - 506, "Maybe lost connection to server"***';
+            // console.log(inputUsd.value = '***Error - 506, "Maybe lost connection to server"***');
         }
     });
 
