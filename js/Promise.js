1456a065cb04ff1d96ff2e6069788fd9cc77344a
Alex <kleykovsky@gmail.com>
2022-08-16 17:10:56 +0500

    rev: 2.1

---
+++ js/Promise.js
@@ -0,0 +1,48 @@
+console.log('Request....');
+
+const req = new Promise(function(resolve, reject) {
+    setTimeout(() => {
+        console.log('Waiting....');
+    
+        const product = {
+            name: 'TV',
+            price: 1000,
+        };
+        resolve(product);
+        // reject();
+    }, 2000);
+});
+req.then((product) => {
+    return new Promise(function(resolve, reject) {
+        setTimeout(() => {
+            product.status = 'order';
+            resolve(product);
+        }, 2000);
+    });
+}).then(data => {
+    data.modify = true;
+    return data;
+}).then((data) => {
+    console.log(data);
+}).catch((error) => {
+    console.log("***error***");
+}).finally(() => {
+    console.log("finally");
+});
+
+// const test = time => {
+//     return new Promise(resolve => {
+//         setTimeout(() => resolve(), time);
+//     });
+// };
+
+// test(1000).then(() => console.log('1000 ms'));
+// test(2000).then(() => console.log('2000 ms'));
+
+// Promise.all([test(1000), test(2000)]).then(() => {
+//     console.log('All');
+// });
+
+// Promise.race([test(1000), test(2000)]).then(() => {
+//     console.log('All');
+// });
--- js/json.js
+++ js/json.js
@@ -9,8 +9,8 @@ const person = {
         dad: 'aaaa'
     }
 };
-
-const clone = JSON.parse(JSON.stringify(person));
+const sendServer = (JSON.stringify(person)); //Отправляем на сервер - парсим
+const clone = JSON.parse(JSON.stringify(person)); //Получаем данные с сервера - распарсиваем
 clone.parents.mom = 'Ann';
 console.log(person);
 console.log(clone);
