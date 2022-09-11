eb41e9e77b5595890fadc6de50d89e5b4452f7d1
Alex <kleykovsky@gmail.com>
2022-09-12 01:54:12 +0500

    rev: 3.7

---
+++ js/regExp.js
@@ -0,0 +1,39 @@
+// new RegExp('pattern', 'flags');
+//    /pattern/f
+
+// методы
+
+// const ans = prompt('Введите ваше имя');
+
+// const  reg = /\d/g;
+// console.log(ans.match(reg));
+
+const str = 'My name is R2D2';
+console.log(str.match(/\D/ig));
+
+// \D не числа
+// \W не буквы
+
+// \d исчем цифры
+// \w все слова и буквы
+// \s пробелы
+
+// flags
+// i
+// g
+// m
+
+
+
+// console.log(ans.search(reg));
+// console.log(ans.match(reg));
+
+// const pass = prompt('Password');
+// console.log(pass.replace(/./g,'*'));
+// let a = pass.replace(/./g, '#');
+
+// console.log('12-34-56'.replace(/-/g, ':'));
+
+console.log();
+
+
