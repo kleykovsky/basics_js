466e1202aee504b30f9ee07623b6d5e612690b19
Alex <kleykovsky@gmail.com>
2022-09-26 02:39:13 +0500

    rev: 3.8

--- js/event_Loop.js
+++ js/event_Loop.js
@@ -1,11 +1,26 @@
-console.log(1);
+// console.log(1);
+
+// setTimeout(() => {
+//     console.log('timeout_2000');
+// }, 2000);
+//
+// setTimeout(() => {
+//     console.log('timeout_4000');
+// }, 4000);
+//
+// console.log(2);
+
+// let k = 0;
+// function count() {
+//     for (let i = 0; i < 1e9; i++){
+//         k++;
+//     }
+//     console.log('done');
+// }
+// count();
 
 setTimeout(() => {
-    console.log('timeout_2000');
-}, 2000);
-
-setTimeout(() => {
-    console.log('timeout_4000');
-}, 4000);
+    console.log(1);
+}, 0);
 
 console.log(2);
