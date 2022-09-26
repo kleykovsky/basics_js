a76980eb331112c0e19fec32f5d78275bae31e66
Alex <kleykovsky@gmail.com>
2022-09-26 18:43:59 +0500

    rev: 4.1

--- js/rendom.js
+++ js/rendom.js
@@ -40,19 +40,19 @@
 
 //random
 
-// // let max = 290,
-// //     min = 180;
+// let max = 290,
+//     min = 180;
 
-// let max = 240,
-//     min = 112;
+let max = 240,
+    min = 112;
 
-//     let randomColor = () => {
-//         return `#${(Math.floor(Math.random() * (max - min) + min))}`;
-//     };
-// let color = randomColor();
-// // let c2 = `#${c()}`;
-// // console.log(c2);
-// // console.log(typeof(c2));
+    let randomColor = () => {
+        return `#${(Math.floor(Math.random() * (max - min) + min))}`;
+    };
+let color = randomColor();
+let c2 = `#${c()}`;
+console.log(c2);
+console.log(typeof(c2));
 /////////////////////////////////////////
 
 //Параметры документа, окна и работа с ними
