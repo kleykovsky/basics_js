f042d1840f6242130e44e2b958c37a1aaae17a1d
Alex <kleykovsky@gmail.com>
2022-09-28 18:33:36 +0500

    rev: 4.2

---
+++ js/map.js
@@ -0,0 +1,27 @@
+let shops = [
+    {rise: 500},
+    {oil: 200},
+    {bred: 50},
+]
+
+let budget = [5000, 15000, 25000];
+
+let map = new Map([
+    [{paper: 4000}, 9000]
+]);
+
+
+shops.forEach((shops, i) => {
+    map.set(shops, budget[i]);
+})
+
+console.log(map);
+
+console.log(map.get(shops[0]));
+console.log(map.has(shops[0]));
+// map.set(shops[0], 5000);
+// map.set(shops[1], 15000);
+// map.set(shops[2], 25000);
+// map.delete();
+// map.clear();
+// map.size;
